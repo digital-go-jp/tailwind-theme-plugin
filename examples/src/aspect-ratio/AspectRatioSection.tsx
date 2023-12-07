021d4fa5006eb7c175de95d1928583e23445b3f8
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-12-07 14:20:15 +0900

    update examples

--- examples/src/aspect-ratio/AspectRatioSection.tsx
+++ examples/src/aspect-ratio/AspectRatioSection.tsx
@@ -1,23 +1,23 @@
 export const AspectRatioSection = () => {
   return (
-    <section className="flex flex-col gap-8">
+    <section className='flex flex-col gap-8'>
       <div>
-        <h2 className="font-bold text-2xl">Aspect Ratio</h2>
+        <h2 className='font-bold text-2xl'>Aspect Ratio</h2>
       </div>
-      <div className="flex flex-wrap gap-8">
-        <div className="flex flex-col gap-2">
-          <div className="bg-solid-grey-400 aspect-16/9 w-[300px]"></div>
-          <span className="text-dns-14N-3">16:9</span>
+      <div className='flex flex-wrap gap-8'>
+        <div className='flex flex-col gap-2'>
+          <div className='bg-solid-grey-400 aspect-16/9 w-[300px]' />
+          <span className='text-dns-14N-3'>16:9</span>
         </div>
 
-        <div className="flex flex-col gap-2">
-          <div className="bg-solid-grey-400 aspect-3/2 w-[300px]"></div>
-          <span className="text-dns-14N-3">3:2</span>
+        <div className='flex flex-col gap-2'>
+          <div className='bg-solid-grey-400 aspect-3/2 w-[300px]' />
+          <span className='text-dns-14N-3'>3:2</span>
         </div>
 
-        <div className="flex flex-col gap-2">
-          <div className="bg-solid-grey-400 aspect-1/1 w-[300px]"></div>
-          <span className="text-dns-14N-3">1:1</span>
+        <div className='flex flex-col gap-2'>
+          <div className='bg-solid-grey-400 aspect-1/1 w-[300px]' />
+          <span className='text-dns-14N-3'>1:1</span>
         </div>
       </div>
     </section>
--- examples/src/radius/RadiusSection.tsx
+++ examples/src/radius/RadiusSection.tsx
@@ -1,29 +1,29 @@
 export const RadiusSection = () => {
   return (
-    <section className="flex flex-col gap-8">
+    <section className='flex flex-col gap-8'>
       <div>
-        <h2 className="text-std-24B-5">Radius</h2>
+        <h2 className='text-std-24B-5'>Radius</h2>
       </div>
-      <div className="flex flex-wrap gap-8">
-        <div className="flex flex-col gap-2">
-          <div className={`h-14 w-14 bg-solid-grey-400`}></div>
-          <span className="text-dns-14N-3">none</span>
+      <div className='flex flex-wrap gap-8'>
+        <div className='flex flex-col gap-2'>
+          <div className={'h-14 w-14 bg-solid-grey-400'} />
+          <span className='text-dns-14N-3'>none</span>
         </div>
-        <div className="flex flex-col gap-2">
-          <div className={`h-14 w-14 bg-solid-grey-400 rounded-8`}></div>
-          <span className="text-dns-14N-3">8px</span>
+        <div className='flex flex-col gap-2'>
+          <div className={'h-14 w-14 bg-solid-grey-400 rounded-8'} />
+          <span className='text-dns-14N-3'>8px</span>
         </div>
-        <div className="flex flex-col gap-2">
-          <div className={`h-14 w-14 bg-solid-grey-400 rounded-16`}></div>
-          <span className="text-dns-14N-3">16px</span>
+        <div className='flex flex-col gap-2'>
+          <div className={'h-14 w-14 bg-solid-grey-400 rounded-16'} />
+          <span className='text-dns-14N-3'>16px</span>
         </div>
-        <div className="flex flex-col gap-2">
-          <div className={`h-14 w-14 bg-solid-grey-400 rounded-32`}></div>
-          <span className="text-dns-14N-3">32px</span>
+        <div className='flex flex-col gap-2'>
+          <div className={'h-14 w-14 bg-solid-grey-400 rounded-32'} />
+          <span className='text-dns-14N-3'>32px</span>
         </div>
-        <div className="flex flex-col gap-2">
-          <div className={`h-14 w-14 bg-solid-grey-400 rounded-full`}></div>
-          <span className="text-dns-14N-3">full</span>
+        <div className='flex flex-col gap-2'>
+          <div className={'h-14 w-14 bg-solid-grey-400 rounded-full'} />
+          <span className='text-dns-14N-3'>full</span>
         </div>
       </div>
     </section>

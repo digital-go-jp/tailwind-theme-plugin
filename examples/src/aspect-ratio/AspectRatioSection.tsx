e27d8db87087a3843cd8b97601b0e788582993c9
keisuke.okafuji <keiokafuj@digital.go.jp>
2024-10-01 11:24:53 +0900

    chore: update token name in example

--- examples/src/aspect-ratio/AspectRatioSection.tsx
+++ examples/src/aspect-ratio/AspectRatioSection.tsx
@@ -2,22 +2,22 @@ export const AspectRatioSection = () => {
   return (
     <section className='flex flex-col gap-8'>
       <div>
-        <h2 className='font-bold text-2xl'>Aspect Ratio</h2>
+        <h2 className='text-std-28B-150'>Aspect Ratio</h2>
       </div>
       <div className='flex flex-wrap gap-8'>
         <div className='flex flex-col gap-2'>
-          <div className='bg-solid-grey-400 aspect-16/9 w-[300px]' />
-          <span className='text-dns-14N-3'>16:9</span>
+          <div className='bg-solid-gray-400 aspect-16/9 w-[300px]' />
+          <span className='text-dns-14N-130'>16:9</span>
         </div>
 
         <div className='flex flex-col gap-2'>
-          <div className='bg-solid-grey-400 aspect-3/2 w-[300px]' />
-          <span className='text-dns-14N-3'>3:2</span>
+          <div className='bg-solid-gray-400 aspect-3/2 w-[300px]' />
+          <span className='text-dns-14N-130'>3:2</span>
         </div>
 
         <div className='flex flex-col gap-2'>
-          <div className='bg-solid-grey-400 aspect-1/1 w-[300px]' />
-          <span className='text-dns-14N-3'>1:1</span>
+          <div className='bg-solid-gray-400 aspect-1/1 w-[300px]' />
+          <span className='text-dns-14N-130'>1:1</span>
         </div>
       </div>
     </section>

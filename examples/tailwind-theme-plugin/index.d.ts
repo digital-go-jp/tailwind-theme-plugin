5b5f1ba3113c2b45a9476d386f80e626227e4120
keisuke.okafuji <keiokafuj@digital.go.jp>
2024-11-20 14:44:53 +0900

    feat: put compiled files under the `examples` directory and use them

---
+++ examples/tailwind-theme-plugin/index.d.ts
@@ -0,0 +1,5 @@
+declare const daPlugin: {
+    handler: import("tailwindcss/types/config").PluginCreator;
+    config?: Partial<import("tailwindcss/types/config").Config>;
+};
+export default daPlugin;
--- examples/tailwind.config.js
+++ examples/tailwind.config.js
@@ -11,6 +11,9 @@ export default {
       }
     },
   },
-  plugins: [require("../dist/index")],
+  // NOTE: 実際に使用する際は @digital-go-jp/tailwind-theme-plugin パッケージをインストールしてお使いください
+  // plugins: [require('@digital-go-jp/tailwind-theme-plugin')],
+  plugins: [require("./tailwind-theme-plugin/index")],
+  
 }
 
